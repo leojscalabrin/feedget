0065a3269143f8f0b5269e51abfe6943ef438958
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-06-12 15:01:39 -0300

    comunicacao components

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,64 @@
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from '../../assets/erro.png';
+import ideaImageUrl from '../../assets/lampada-de-ideia.png';
+import thoughtImageUrl from '../../assets/nuvem.png';
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedBackContentStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            title: 'erro icones',
+            alt: 'Erro ícones criados por Dave Gandy - Flaticon'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            title: 'lâmpada icones',
+            alt: 'Lâmpada ícones criados por Icon home - Flaticon'
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            title: 'nuvem ícones',
+            alt: 'Nuvem ícones criados por DinosoftLabs - Flaticon'
+        }
+    },
+}
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        {!feedbackType ? (
+             <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+        ) : (
+            <FeedbackContentStep
+             feedbackType={feedbackType}
+             onFeedbackRestartRequested={handleRestartFeedback}
+             />
+        )
+
+        }
+
+           <footer>
+                Isto é um footer
+           </footer>
+        </div>
+    )
+}
